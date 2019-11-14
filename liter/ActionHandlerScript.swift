49a6ff45f70695023c9a483923eb5313e46b1e9d
Joe Walsh <joewalsh@users.noreply.github.com>
2019-11-14 10:33:55 -0500

    fix theme

--- liter/ActionHandlerScript.swift
+++ liter/ActionHandlerScript.swift
@@ -2,7 +2,13 @@ import WebKit
 
 final class ActionHandlerScript: WKUserScript   {
     required init(theme: String, messageHandlerName: String, interfaceName: String) {
-        let setupParams: String = "{theme: '\(interfaceName)_theme_\(theme.lowercased())', margins: {top: '16px', right: '16px', bottom: '16px', left: '16px'}, areTablesInitiallyExpanded: true}"
+        let themeClass: String
+        if interfaceName == "pcs" {
+            themeClass = "pcs-theme-\(theme.lowercased())"
+        } else {
+            themeClass = "pagelib_theme_\(theme.lowercased())"
+        }
+        let setupParams: String = "{theme: '\(themeClass)', margins: {top: '16px', right: '16px', bottom: '16px', left: '16px'}, areTablesInitiallyExpanded: true}"
         let source = """
         document.pcsActionHandler = (action) => {
           window.webkit.messageHandlers.\(messageHandlerName).postMessage(action)
